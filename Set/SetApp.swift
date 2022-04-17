4a923935202e8ad94f296201a9213cefe7f90acb
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-04-17 15:15:32 +0200

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Sebastian Pfeufer on 17.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
